7fbcc08c8977dd71c3207098c7e9ec8a7ef43f54
luiztestonidev <luiztestoni5@gmail.com>
2026-05-25 21:33:06 -0300

    feat: implementa classe leitor e classes extends LeitorComum e LeitorPremium

--- src/Leitores/Leitor.java
+++ src/Leitores/Leitor.java
@@ -1,5 +1,45 @@
 package Leitores;
 
-public class Leitor {
+public abstract class Leitor {
 
+    private String nome;
+    private int idade;
+    private double multaPendente;
+
+    public Leitor(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public abstract int getLimiteEmprestimos();
+
+    public boolean podeEmprestar(int emprestimosAtivos) {
+
+        return emprestimosAtivos < getLimiteEmprestimos()
+                && multaPendente <= 0;
+    }
+
+    public boolean possuiMulta() {
+        return multaPendente > 0;
+    }
+
+    public void adicionarMulta(double valor) {
+        multaPendente += valor;
+    }
+
+    public void quitarMulta() {
+        multaPendente = 0;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getMultaPendente() {
+        return multaPendente;
+    }
 }
